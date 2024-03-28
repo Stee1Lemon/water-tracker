c9dbdbf0bf3b16842b4ace8828df7bf749bf8ea5
Stee1Lemon <121914099+Stee1Lemon@users.noreply.github.com>
2024-03-28 23:52:00 +0000

    test-env

--- src/pages/FirstPage/FirstPage.jsx
+++ src/pages/FirstPage/FirstPage.jsx
@@ -1,11 +1,13 @@
 import { Container, Title, StyledImage } from './FirstPage.styled';
 import example from '../../assets/example.png';
-import { log } from 'console';
 
 const FirstPage = () => {
+  const test = import.meta.env.VITE_API_TEST;
+
   return (
     <Container>
       <Title>First Page Test env</Title>
+      <p>${test}</p>
       <StyledImage src={example} alt="Example" />
     </Container>
   );
