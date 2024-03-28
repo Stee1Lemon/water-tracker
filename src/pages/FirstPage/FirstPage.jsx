7890c29208763f8d55d6e8aa2b9b36ebca0973fe
Stee1Lemon <121914099+Stee1Lemon@users.noreply.github.com>
2024-03-28 23:49:13 +0000

    test-env

--- src/pages/FirstPage/FirstPage.jsx
+++ src/pages/FirstPage/FirstPage.jsx
@@ -3,10 +3,9 @@ import example from '../../assets/example.png';
 import { log } from 'console';
 
 const FirstPage = () => {
-  log('first page');
   return (
     <Container>
-      <Title>First Page</Title>
+      <Title>First Page Test env</Title>
       <StyledImage src={example} alt="Example" />
     </Container>
   );
