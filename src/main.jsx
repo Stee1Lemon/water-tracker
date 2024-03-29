83c7211a52b3030e84bf0ad83cc106aeb91fda86
Stee1Lemon <121914099+Stee1Lemon@users.noreply.github.com>
2024-03-29 14:48:21 +0000

    redux-persist

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { Provider } from 'react-redux';
-import store from './redux/store.js';
+import { persistor, store } from './redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/water-tracker">
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/water-tracker">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,32 @@
 import { configureStore } from '@reduxjs/toolkit';
 import testReducer from './test/test-slice.js';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-export default configureStore({
+const testPersistConfig = {
+  key: 'test',
+  storage,
+};
+
+export const store = configureStore({
   reducer: {
-    test: testReducer,
+    test: persistReducer(testPersistConfig, testReducer),
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
