4dc7170e1fe6535e12fb6d2a0349b293a0bc9f1b
iratololo <iratololo1991@gmail.com>
2024-03-31 14:56:22 +0300

    fix

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,6 @@
 import { Container } from 'components/Container/Container';
 import { MonthStatsTable } from "components/Home/MonthStatsTable/MonthStatsTable"
 import { TodayWaterList } from "components/Home/TodayWaterList/TodayWaterList"
-import { Test } from 'components/Test/Test';
 
 import {Statistics} from "./HomePage.styled"
 
@@ -10,7 +9,6 @@ const HomePage = () => {
     <>
       <Container>
         <h2>Home Page</h2>
-        <Test />
         <Statistics>
           <TodayWaterList/>
           <MonthStatsTable/>
