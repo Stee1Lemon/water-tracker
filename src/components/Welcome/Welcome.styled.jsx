712152dbd8f80a8857aeb0c9eba8584e7d4e48fb
Kamila0301 <bogda0301@gmail.com>
2024-04-04 13:25:19 +0300

    adaptive layout + routing

--- src/components/Welcome/Welcome.styled.jsx
+++ src/components/Welcome/Welcome.styled.jsx
@@ -1,49 +1,121 @@
 import styled from 'styled-components';
-
-export const BackgroundImgWelcome = styled.div`
-  background-repeat: no-repeat;
-`;
-
-export const WelcomePageImage = styled.img`
-  width: 100%;
-  height: 100%;
-`;
+import { NavLink } from 'react-router-dom';
 
 export const WelcomePageWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: normal;
+  @media screen and (max-width: 767px) {
+    display: flex;
+    flex-direction: column;
+    align-items: normal;
 
-  margin: 0 auto;
-  padding-top: 60px;
-  padding-bottom: 294px;
-  padding-left: 30px;
-  padding-right: 210px;
+    margin: 0 auto;
+    padding-top: 24px;
+    padding-bottom: 40px;
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+
+  @media screen and (min-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    align-items: normal;
+
+    margin: 0 auto;
+    padding-top: 40px;
+    padding-bottom: 50px;
+    padding-left: 32px;
+    padding-right: 32px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    display: flex;
+    flex-direction: row;
+    align-items: normal;
+
+    margin: 0 auto;
+    padding-top: 60px;
+    padding-bottom: 294px;
+    padding-left: 30px;
+    padding-right: 210px;
+  }
 `;
 
 export const WelcomePageWrapperFirst = styled.div`
-  position: relative;
+  @media screen and (max-width: 767px) {
+    position: absolute;
+  }
+  @media screen and (min-width: 768px) {
+    position: absolute;
+  }
+  @media screen and (min-width: 1440px) {
+    position: relative;
+  }
 `;
 
 export const WelcomePageTextWrapper = styled.div``;
 
 export const WelcomePageTitle = styled.h1`
-  color: var(--primary-dark);
-  font-family: Roboto;
-  font-size: 36px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 1.2;
+  @media screen and (max-width: 767px) {
+    color: var(--primary-dark);
+    font-family: Roboto;
+    font-size: 28px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 1.1;
+    max-width: 246px;
+  }
+
+  @media screen and (min-width: 768px) {
+    color: var(--primary-dark);
+    font-family: Roboto;
+    font-size: 36px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 1.2;
+  }
+
+  @media screen and (min-width: 1440px) {
+    color: var(--primary-dark);
+    font-family: Roboto;
+    font-size: 36px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 1.2;
+  }
 `;
 export const WelcomePageSubtitle = styled.p`
-  color: var(--primary-dark);
-  font-family: Roboto;
-  font-size: 26px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 32px;
-  margin-top: 16px;
-  margin-bottom: 0;
+  @media screen and (max-width: 767px) {
+    color: var(--primary-dark);
+    font-family: Roboto;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 1.2;
+    margin-top: 16px;
+    margin-bottom: 0;
+    max-width: 197px;
+  }
+
+  @media screen and (min-width: 768px) {
+    color: var(--primary-dark);
+    font-family: Roboto;
+    font-size: 26px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 1.2;
+    margin-top: 16px;
+    margin-bottom: 0;
+  }
+
+  @media screen and (min-width: 1440px) {
+    color: var(--primary-dark);
+    font-family: Roboto;
+    font-size: 26px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 1.2;
+    margin-top: 16px;
+    margin-bottom: 0;
+  }
 `;
 
 export const WelcomePageTrackerTitle = styled.h2`
@@ -54,70 +126,212 @@ export const WelcomePageTrackerTitle = styled.h2`
   font-weight: 500;
   line-height: 1.1;
   margin-top: 24px;
+  margin-bottom: 0;
 `;
+
 export const WelcomePageTrackerList = styled.ul`
-  padding: 0;
-  padding-bottom: 34px;
+  @media screen and (max-width: 767px) {
+    padding: 0;
+    padding-bottom: 24px;
+  }
+  @media screen and (min-width: 768px) {
+    padding: 0;
+    padding-bottom: 24px;
+    display: flex;
+    flex-direction: row;
+    gap: 100px;
+  }
+  @media screen and (min-width: 1440px) {
+    padding: 0;
+    padding-bottom: 24px;
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+  }
 `;
 
 export const WelcomePageTrackerItem = styled.li`
-  display: flex;
+  @media screen and (max-width: 767px) {
+    display: flex;
 
-  align-items: center;
-  margin-top: 16px;
-  color: var(--primary-dark);
-  font-family: Roboto;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 1.25;
-  svg {
-    width: 40px;
-    height: 40px;
-    stroke: var(--primary-focus);
-    fill: none;
-    margin-right: 8px;
+    align-items: center;
+    margin-top: 12px;
+
+    color: var(--primary-dark);
+    font-family: Roboto;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 1.2;
+
+    svg {
+      width: 32px;
+      height: 32px;
+      stroke: var(--primary-focus);
+      fill: none;
+      margin-right: 8px;
+    }
+  }
+
+  @media screen and (min-width: 768px) {
+    display: flex;
+
+    align-items: center;
+    margin-top: 12px;
+
+    color: var(--primary-dark);
+    font-family: Roboto;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 1.2;
+    svg {
+      width: 40px;
+      height: 40px;
+      stroke: var(--primary-focus);
+      fill: none;
+      margin-right: 8px;
+    }
+  }
+
+  @media screen and (min-width: 1440px) {
+    display: flex;
+
+    align-items: center;
+    margin-top: 16px;
+    color: var(--primary-dark);
+    font-family: Roboto;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 1.25;
+    svg {
+      width: 40px;
+      height: 40px;
+      stroke: var(--primary-focus);
+      fill: none;
+      margin-right: 8px;
+    }
   }
 `;
 
 export const WelcomePageButton = styled.button`
-  display: flex;
-  height: 44px;
-  width: 384px;
-  padding: 10px 30px;
-  justify-content: center;
-  align-items: center;
-  gap: 10px;
-  border-radius: 10px;
-  border: none;
-  background: var(--primary-focus);
-  box-shadow: 0px 4px 8px 0px rgba(64, 123, 255, 0.34);
+  @media screen and (max-width: 767px) {
+    display: flex;
+    height: 36px;
+    width: 280px;
+    padding: 8px 30px;
+    justify-content: center;
+    align-items: center;
 
+    border-radius: 10px;
+    border: none;
+    background: var(--primary-focus);
+    box-shadow: 0px 4px 8px 0px rgba(64, 123, 255, 0.34);
+
+    color: var(--primary-light);
+    text-align: center;
+    font-family: Roboto;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 1.2;
+  }
+
+  @media screen and (min-width: 768px) {
+    display: flex;
+    height: 44px;
+    width: 336px;
+    padding: 10px 30px;
+    justify-content: center;
+    align-items: center;
+
+    border-radius: 10px;
+    border: none;
+    background: var(--primary-focus);
+    box-shadow: 0px 4px 8px 0px rgba(64, 123, 255, 0.34);
+    margin-bottom: 60px;
+
+    color: var(--primary-light);
+    text-align: center;
+    font-family: Roboto;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 1.3;
+  }
+
+  @media screen and (min-width: 1440px) {
+    display: flex;
+    height: 44px;
+    width: 384px;
+    padding: 10px 30px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    border-radius: 10px;
+    border: none;
+    background: var(--primary-focus);
+    box-shadow: 0px 4px 8px 0px rgba(64, 123, 255, 0.34);
+
+    color: var(--primary-light);
+    text-align: center;
+    font-family: Roboto;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 1.3;
+  }
+`;
+
+export const WelcomePageNavLink = styled(NavLink)`
   color: var(--primary-light);
-  text-align: center;
-  font-family: Roboto;
-  font-size: 18px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: 1.3;
+
+  
+    
+  }
 `;
 
 export const WelcomePageDescriptionWrapper = styled.div`
   position: relative;
-
+  display: flex;
   flex-direction: column;
   align-items: flex-start;
 `;
 
 export const WelcomePageDescriptionDiv = styled.div`
-  position: absolute;
-  padding: 32px 21px;
-  border-radius: 10px;
-  background: var(--secondary-first);
-  box-shadow: 0px 4px 14px 0px rgba(64, 123, 255, 0.3);
-  margin-left: 81px;
-  margin-top: 63px;
-  width: 515px;
+  @media screen and (max-width: 767px) {
+    position: absolute;
+    padding: 24px 16px;
+    border-radius: 10px;
+    background: var(--secondary-first);
+    box-shadow: 0px 4px 14px 0px rgba(64, 123, 255, 0.3);
+    margin-left: 0px;
+    margin-top: 420px;
+    width: 280px;
+  }
+
+  @media screen and (min-width: 768px) {
+    position: absolute;
+    padding: 32px 24px;
+    border-radius: 10px;
+    background: var(--secondary-first);
+    box-shadow: 0px 4px 14px 0px rgba(64, 123, 255, 0.3);
+
+    margin-top: 320px;
+    width: 500px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    position: absolute;
+    padding: 32px 21px;
+    border-radius: 10px;
+    background: var(--secondary-first);
+    box-shadow: 0px 4px 14px 0px rgba(64, 123, 255, 0.3);
+    margin-left: 81px;
+    margin-top: 78px;
+    width: 515px;
+  }
 `;
 
 export const WelcomePageDescriptionTitle = styled.h2`
@@ -130,11 +344,29 @@ export const WelcomePageDescriptionTitle = styled.h2`
   margin-bottom: 12px;
 `;
 export const WelcomePageDescriptionList = styled.ul`
-  list-style: initial;
+  @media screen and (max-width: 767px) {
+    list-style: initial;
 
-  text-align: left;
-  color: var(--primary-focus);
-  padding-left: 24px;
+    text-align: left;
+    color: var(--primary-focus);
+    padding-left: 16px;
+  }
+
+  @media screen and (min-width: 768px) {
+    list-style: initial;
+
+    text-align: left;
+    color: var(--primary-focus);
+    padding-left: 24px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    list-style: initial;
+
+    text-align: left;
+    color: var(--primary-focus);
+    padding-left: 24px;
+  }
 `;
 
 export const WelcomePageDescriptionItem = styled.li`
@@ -143,6 +375,7 @@ export const WelcomePageDescriptionItem = styled.li`
   font-size: 16px;
   font-style: normal;
   font-weight: 400;
+  line-height: 1.2;
 
   margin-bottom: 8px;
 
--- src/pages/WelcomePage/WelcomePage.styled.jsx
+++ src/pages/WelcomePage/WelcomePage.styled.jsx
@@ -2,43 +2,84 @@ import styled from 'styled-components';
 
 import DesktopBackground1 from '../../assets/WelcomePage/background-main-page.png';
 import DesktopElement from '../../assets/WelcomePage/background-element-main-page1.png';
-// import TabletBackground1 from '../../assets/WelcomePage/tablet-background-main-page.png';
-// import TabletElement from '../../assets/WelcomePage/tablet-background-element-home-screen.png';
-// import MobileBackground1 from '../../assets/WelcomePage/mobile-background-main-page.png';
+import TabletBackground1 from '../../assets/WelcomePage/tablet-background-main-page.png';
+import TabletElement from '../../assets/WelcomePage/tablet-background-element-home-screen.png';
+import MobileBackground1 from '../../assets/WelcomePage/mobile-background-main-page.png';
 
 export const WelcomeSection = styled.section`
-  height: 100%;
-  background-image: url(${DesktopBackground1});
-  background-size: contain;
-  background-repeat: no-repeat;
-  background-position: center bottom;
-  &::after {
-    content: '';
-    position: absolute;
-    background-image: url(${DesktopElement});
+  @media only screen and (max-width: 767px) {
+    background-image: url(${MobileBackground1});
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center bottom;
+  }
+
+  @media only screen and (min-width: 768px) {
+    height: 100%;
+    background-image: url(${TabletBackground1});
     background-size: contain;
     background-repeat: no-repeat;
-    z-index: -99;
-    top: 50%;
-    left: 50%;
-    transform: translate(-48%, -48%);
-    width: 1404px;
-    height: 582px;
+    background-position: center bottom;
+    width: 780px;
+    height: 800px;
+    &::after {
+      content: '';
+      position: absolute;
+      background-image: url(${TabletElement});
+      background-size: contain;
+      background-repeat: no-repeat;
+      z-index: -99;
+      top: 50%;
+      left: 50%;
+      transform: translate(-48%, -48%);
+      width: 780px;
+      height: 800px;
+    }
+  }
+
+  @media only screen and (min-width: 1440px) {
+    height: 100%;
+    width: 100%;
+    background-image: url(${DesktopBackground1});
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center bottom;
+
+    &::after {
+      content: '';
+      position: absolute;
+      background-image: url(${DesktopElement});
+      background-size: contain;
+      background-repeat: no-repeat;
+      z-index: -99;
+      top: 50%;
+      left: 50%;
+      transform: translate(-48%, -48%);
+      width: 1404px;
+      height: 582px;
+    }
   }
 `;
 
 export const WelcomeContainer = styled.div`
+  @media screen and (max-width: 767px) {
+    height: 952px;
 
- 
-  
-  height: 600px;
+    max-width: 320px;
+    margin: 0 auto;
+    padding: 20px 40px;
+  }
 
-  
+  @media screen and (min-width: 768px) {
+    height: 800px;
 
+    padding: 16px 32px 50px 32px;
+    margin: 0 auto;
+  }
+
+  @media screen and (min-width: 1440px) {
     max-width: 1280px;
     margin: 0 auto;
-    padding: 20px 112px;
-   
-    
+    padding: 18px 119px;
   }
 `;
