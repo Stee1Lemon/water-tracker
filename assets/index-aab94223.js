79de83594dd5a599e20262ea5599a8f6ea4be045
Stee1Lemon <121914099+Stee1Lemon@users.noreply.github.com>
2024-04-05 18:16:15 +0000

    Deploying to gh-pages from @ Stee1Lemon/water-tracker@b933e920f700298893e5c259fa3a67289a8c9ce9 🚀

--- assets/index-aab94223.js
+++ assets/index-aab94223.js
@@ -1690,12 +1690,10 @@ attempted value: ${a}
   @media screen and (min-width: 768px) {
     max-width: 768px;
     padding: 40px 32px;
-    margin: 0;
   }
 
   @media screen and (min-width: 1440px) {
     max-width: 1280px;
-    margin: 0 auto;
     padding: 20px 112px;
     display: flex;
     justify-content: flex-end;
@@ -1898,12 +1896,10 @@ attempted value: ${a}
   @media screen and (min-width: 768px) {
     max-width: 768px;
     padding: 40px 32px;
-    margin: 0;
   }
 
   @media screen and (min-width: 1440px) {
     max-width: 1280px;
-    margin: 0 auto;
     padding: 20px 112px;
     display: flex;
     justify-content: flex-end;
