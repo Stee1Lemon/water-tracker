76bdaee97d3a0fd04f41200025819f94d2d701a7
shtrvv <shatrova.liza.21@gmail.com>
2024-04-05 20:52:56 +0300

    fix forms position

--- src/pages/SignupPage/SignupPage.styled.jsx
+++ src/pages/SignupPage/SignupPage.styled.jsx
@@ -78,12 +78,10 @@ export const FormContainer = styled.div`
   @media screen and (min-width: 768px) {
     max-width: 768px;
     padding: 40px 32px;
-    margin: 0;
   }
 
   @media screen and (min-width: 1440px) {
     max-width: 1280px;
-    margin: 0 auto;
     padding: 20px 112px;
     display: flex;
     justify-content: flex-end;
