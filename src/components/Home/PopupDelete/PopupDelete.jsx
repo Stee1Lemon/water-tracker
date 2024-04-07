7478afc096147943d39ed089976170afdeee8007
iratololo <iratololo1991@gmail.com>
2024-04-07 19:11:07 +0300

    Delete portion

--- src/components/Home/PopupDelete/PopupDelete.jsx
+++ src/components/Home/PopupDelete/PopupDelete.jsx
@@ -1,11 +1,24 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { useDispatch } from 'react-redux';
+
 import ModalOverlay from 'components/ModalOverlay/ModalOverlay';
+
 import { ModalSubtitle, ModalTitle, ModalCloseButton } from "../CommonStyles.styled"
 import { Popup, PopupFooter, PopupBtnRemove, PopupBtnClose } from "./PopupDelete.styled"
 import icons from '../../../assets/icons.svg';
 
+import waterApi from "../../../redux/water/waterOperations"
+
 export function PopupDelete({ isOpen, onClose, selectedItemId }) {
+    
+    const dispatch = useDispatch();
+    
     const handleDelete = () => {
-        console.log('DELETE PORTION WATER :>> ', {_id:selectedItemId});
+        dispatch(waterApi.deleteWaterThunk({
+        id: selectedItemId,
+        }));
+        Notify.success('The portion is removed');
+        onClose()
     }
     return (
       <ModalOverlay isOpen={isOpen} onClose={onClose}>
