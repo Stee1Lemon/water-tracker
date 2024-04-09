3f494acda8a59ee0748d5dcb20965d845db41b02
iratololo <iratololo1991@gmail.com>
2024-04-09 15:11:48 +0300

    additional conditions for validation

--- src/components/Home/Schema/validationWaterSchema.js
+++ src/components/Home/Schema/validationWaterSchema.js
@@ -11,12 +11,22 @@ export const validationWaterSchema = Yup.object({
   dailyWaterGoal: Yup.number()
     .required('Amount water is required')
     .min(0, 'Amount water must be a positive number')
-    .max(15, 'The amount of water should be no more than 15000 liters'),
+    .max(15, 'The amount of water should be no more than 15000 liters')
+    .test(
+      "maxDigitsAfterDecimal",
+      "The amount of water must have 1 digits after decimal or less",
+      (number) => Number.isInteger(number * (10 ** 1))
+    ),
 });
 
 export const AddWaterSchema = Yup.object({
   portionOfWater: Yup.number()
     .required('Amount water is required')
     .min(0, 'Amount water must be a positive number')
-    .max(5000, 'The amount of water should be no more than 5000 milliliters'),
+    .max(5000, 'The amount of water should be no more than 5000 milliliters')
+    .test(
+      "maxDigitsAfterDecimal",
+      "The amount of water must be an integer",
+      (number) => Number.isInteger(number * (10 ** 0))
+    ),
 });
