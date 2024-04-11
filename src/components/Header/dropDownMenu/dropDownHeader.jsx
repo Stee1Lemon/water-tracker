e21ae2e8e313313322d4a6845a9bd2b470627689
Debeluk <popcorn.starinets@gmail.com>
2024-04-11 17:21:33 +0300

    1

--- src/components/Header/dropDownMenu/dropDownHeader.jsx
+++ src/components/Header/dropDownMenu/dropDownHeader.jsx
@@ -4,10 +4,12 @@ import { ReactComponent as UserLogOut } from '../headerIcons/UserLogOut.svg';
 import LogOutModal from '../logOutModal/logOutModal';
 import { useState } from 'react';
 import SettingsModal from '../settingsModal/settingsModal';
+import { useTranslation } from 'react-i18next';
 
 const DropDownHeader = ({ menuRef, isMenuVisible, toggleDropDown }) => {
   const [isLogoutModalOpen, setLogoutModalOpen] = useState(false);
   const [isSettingsModalOpen, setSettingsModalOpen] = useState(false);
+  const { t } = useTranslation();
 
   const handleLogOutModal = () => {
     toggleDropDown();
@@ -25,13 +27,13 @@ const DropDownHeader = ({ menuRef, isMenuVisible, toggleDropDown }) => {
         <div className="dropDownElement">
           <button className="dropDownButton" onClick={handleSettingsModal}>
             <UserCog className="svgCog" />
-            Setting
+            {t('header.setting')}
           </button>
         </div>
         <div className="dropDownElement" onClick={handleLogOutModal}>
           <button className="dropDownButton">
             <UserLogOut className="svgLogOut" />
-            Log out
+            {t('header.logout')}
           </button>
         </div>
       </DropdownMenu>
--- src/components/Header/logOutModal/logOutModal.jsx
+++ src/components/Header/logOutModal/logOutModal.jsx
@@ -4,9 +4,11 @@ import { useDispatch } from 'react-redux';
 import authApi from '../../../redux/auth/authOperations';
 import { waterSlice } from '../../../redux/water/waterSlice';
 import ModalOverlay from 'components/ModalOverlay/ModalOverlay';
+import { useTranslation } from 'react-i18next';
 
 const LogOutModal = ({ isOpen, onClose }) => {
   const dispatch = useDispatch();
+  const { t } = useTranslation();
 
   const handleLogOut = () => {
     dispatch(authApi.logoutThunk());
@@ -17,18 +19,18 @@ const LogOutModal = ({ isOpen, onClose }) => {
     <ModalOverlay isOpen={isOpen} onClose={onClose}>
       <ModalLogOutContainer>
         <div className="logOutDiv1">
-          <p className="logOutP1">Log out</p>
+          <p className="logOutP1">{t('logoutModal.logout')}</p>
           <Xmark className="xMarkWrapper" onClick={onClose} />
         </div>
         <div className="logOutDiv2">
-          <p className="logOutP2">Do you really want to leave?</p>
+          <p className="logOutP2">{t('logoutModal.question')}</p>
         </div>
         <div className="logOutDiv3">
           <button className="logOutButtonDelete" onClick={handleLogOut}>
-            Log out
+            {t('logoutModal.buttonLogout')}
           </button>
           <button className="logOutButtonCancel" onClick={onClose}>
-            Cancel
+            {t('logoutModal.buttonCancel')}
           </button>
         </div>
       </ModalLogOutContainer>
