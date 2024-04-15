9055390251bce09ea70af51ec7c733de1ac2d54e
Alina <alinatanst@gmail.com>
2024-04-15 18:46:40 +0300

    Summary

--- src/pages/SigninPage/SigninPage.jsx
+++ src/pages/SigninPage/SigninPage.jsx
@@ -1,11 +1,7 @@
 import { SigninForm } from 'components/SigninForm/SigninForm';
 import { FormContainer } from './SigninPage.styled';
-// import { useSearchParams } from 'react-router-dom';
 
 const SigninPage = () => {
-  // const [token, setToken] = useSearchParams();
-  // console.log(token.get('token'))
-
   return (
     <section>
       <FormContainer>
